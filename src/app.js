765339518a95952406b1e6b1af0376bcea527796
facundo vega <facundovega022@gmail.com>
2023-04-24 03:27:45 -0300

    Primera entrega

--- src/app.js
+++ src/app.js
@@ -1,25 +1,13 @@
 import express from 'express'
-import ProductManager from "./management/productManager.js"
+import productRouter from './routers/products.router.js'
+import cartRouter from './routers/carts.router.js'
 
 const app= express();
-const manager= new ProductManager();
 
+app.use(express.json())
 
-app.get('/products',(req, res)=>{
-
-    const limit = req.query.limit
-    let list= manager.getProducts()
-    if(limit) {
-        list = list.slice(0, limit)
-    } 
-    res.send(list)
-} )
-
-app.get('/products/:pid', (req, res)=>{
-    const pid= req.params.pid
-    const identified= manager.getProductsById(+pid)
-    res.send(identified)
-})
+app.use('/api/products', productRouter)
+app.use('/api/carts', cartRouter)
 
 app.listen(8080, ()=>console.log("server up"))
 
--- src/management/productManager.js
+++ src/management/productManager.js
@@ -4,45 +4,49 @@ import fs from 'fs'
 class ProductManager{
 
     constructor (){
-        this.products= []
-        this.path='products.json'
+        this.path='./src/data/products.json'
     }
     getProducts= ()=>{
-        // const productsList = JSON.parse(fs.readFileSync(this.path, "utf-8"))
-        const productsList = JSON.parse(fs.readFileSync('./productitos.json', "utf-8"))
+        const productsList = JSON.parse(fs.readFileSync(this.path, "utf-8"))
         return(productsList);
        
     }
-
+    generateRandomString = () =>{
+        const charsAvaible="ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+        let string = "";
+        for (let i = 0; i < 6; i++) {
+            string += charsAvaible.charAt(Math.floor(Math.random() * charsAvaible.length));
+        }
+        return string;
+    }
     generateId= ()=>{
         let list= this.getProducts()
         if(list.length === 0) return 1
         return list[list.length-1].id +1
     }
 
-    validateCode= (code)=>{
-        let list= this.getProducts()
-        const auxCode= list.find(x => x.code == code )
-        if(auxCode!=undefined) return "invalid code"
+    validateCode= ()=>{
+        const list= this.getProducts()
+        let code;
+        let auxCode;
+        do {
+            code = this.generateRandomString()
+            auxCode= list.find(x => x.code == code )
+        } while(auxCode != undefined)
+
         return code
     }
 
-    addProducts=(title, description, price, thumbnail,code, stock) =>{
-        try {
+    addProducts=({title, description, price, category,  thumbnail, stock}) =>{
             let list= this.getProducts()
             const id= this.generateId()
-            code = this.validateCode(code)
-            const product={id, title, description, price, thumbnail, code, stock}
+            const code = this.validateCode()
+            thumbnail ? thumbnail : thumbnail="empty"
+            const product={id, title, description, price ,status:true ,category, thumbnail, code, stock}
             list.push(product)
             fs.writeFileSync(this.path, JSON.stringify(list, null))
             console.log(`Product with id ${id} has been created`)
-            
-        } catch (error) {
-            console.log(error)
-        }
     }
-
-    
 
     getProductsById= (id)=>{
         
@@ -58,26 +62,27 @@ class ProductManager{
         }
     }
 
-    updateProduct= (id, key, value)=>{
+    updateProduct= (id, data)=>{
         
-        try {
-            let list=  this.getProducts()
+            let list= this.getProducts()
             const pIndex= list.findIndex(p => p.id == id)
-
-            if(pIndex != -1){
-                list[pIndex][key]= value
-                fs.writeFile(this.path, JSON.stringify(list, null, 2)) 
-                console.log(`Product with id ${id} has been updated`)
-            }else{
-                console.log(`There isn't any product whose id is ${id}`)
-            }
-
+            const original = this.getProductsById(id)
             
-        } catch (error) {
-            console.log(error)
-        }
-
+            if(original.id == id ){
+                
+                if(pIndex != -1){
+                    list[pIndex]={...list[pIndex], ...data}
+                    
+                    fs.writeFileSync(this.path, JSON.stringify(list, null, 2)) 
+                    console.log(`Product with id ${id} has been updated`)
+                }else{
+                    console.log(`There isn't any product whose id is ${id}`)
+                }
+            }else{
+                console.log("The field id and code can't be modified")
+            }
     }
+
     deleteProduct =(id) => {      
         try {
             const PTDelete=  this.getProductsById(id)
@@ -86,15 +91,14 @@ class ProductManager{
                 console.log("id not found")
             }
             list = list.filter(prod => prod.id != PTDelete.id) 
-            fs.writeFile(this.path, JSON.stringify(list, null, 2)) 
+            fs.writeFileSync(this.path, JSON.stringify(list, null, 2)) 
             console.log(`product with id ${id} has been eliminated`)
         } catch (error) {
             console.log(error)
         }
     }
 }
-const manager= new ProductManager()
-//manager.updateProduct(1, "stock", 7 )
-manager.addProducts("azucar", "Endulzante natural", 550, null, "A202", 2)
+//const manager= new ProductManager()
+//manager.addProducts("cafe", "colombiano, aroma suave",1000, null, "A101", 5)
 //manager.deleteProduct(4)
 export default ProductManager
